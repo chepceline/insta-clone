92664bb0b7bb00ea0aeb3970e7cf1741d0106c42
chepceline <chepceline25@gmail.com>
2022-06-05 08:20:32 +0300

    add changes

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -123,6 +123,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR/'static'
+]
+
+MEDIA_ROOT=BASE_DIR/'static/images'
+STATIC_ROOT = BASE_DIR/'staticfiles'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = '/user/login/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

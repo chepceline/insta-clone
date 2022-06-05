38d6a72d7ab660b731325756498cdfb2814fe02d
chepceline <chepceline25@gmail.com>
2022-06-05 17:09:36 +0300

    register the paths

--- insta/admin.py
+++ insta/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post, Profile, Comment, Like, Follow
 
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Follow)
+

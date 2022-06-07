8c7ea173e172f46d94459fcceea1f96800fe388b
chepceline <chepceline25@gmail.com>
2022-06-07 10:10:33 +0300

    register posts

--- insta/admin.py
+++ insta/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Post, Profile, Comment, Like, Follow
-
 # Register your models here.
 
 admin.site.register(Post)
@@ -8,4 +7,3 @@ admin.site.register(Profile)
 admin.site.register(Comment)
 admin.site.register(Like)
 admin.site.register(Follow)
-

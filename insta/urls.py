23a25acb51b08d21969926de7d879de844eaf491
chepceline <chepceline25@gmail.com>
2022-06-07 10:07:33 +0300

    add urls

--- insta/urls.py
+++ insta/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path, include
+from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
+from django.urls import path, include
 
+from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -14,8 +16,5 @@ urlpatterns = [
     path('comment/<post_id>', views.comment, name='comment'),
     path('like/<post_id>', views.like, name='like'),
     path('follow/<user_id>', views.follow, name='follow'),
-    
-   
+
 ]
-
-
